3b697108b5bf094e45bb3e65aed90719a3a922a2
Arafat Rahman <arafat.rahman.6003@gmail.com>
2026-05-29 16:56:40 +0600

    feat: implement chat, message, and user retrieval controllers and routes

--- backend/src/routes/chatRouter.ts
+++ backend/src/routes/chatRouter.ts
@@ -1,9 +1,12 @@
-import { Router } from "express"
+import { Router } from "express";
+import { protectRoute } from "../middlewares/auth";
+import { getChats, getOrCreateChat } from "../controllers/chatController";
 
 const router = Router();
 
+router.use(protectRoute);
 
-
-
+router.get("/", getChats);
+router.post("/with/:participantId", getOrCreateChat);
 
 export default router;
